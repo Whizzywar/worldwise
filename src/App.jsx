e32645df7bb7b75a895dce0e1b61a3a4644b5771
Whizzyware <ubaniwisdom480@gmail.com>
2025-11-18 04:52:28 +0100

    added task

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="product" element={<Product />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>list of cities</p>} />
+          <Route path="cities" element={<p>list of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
